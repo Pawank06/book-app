cace1d3f201929df9f92d1cbff8e0f28833ebd53
PawanK06 <pawan06kumar2003@gmail.com>
2024-06-08 13:39:18 +0530

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import { config } from "./config/config";
+import globalErrorhandler from "./middlewares/globalErrorhandler";
 
-const app = express()
+const app = express();
 
+app.get("/", (req, res, next) => {
+  
+  res.json({ message: "Welcome" });
+});
 
-app.get('/', (req, res, next) => {
-    res.json({message: "Welcome"})
-})
+// Global error handler
 
-export default app
+app.use(globalErrorhandler);
+
+export default app;
---
+++ src/middlewares/globalErrorhandler.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+import { config } from "../config/config";
+
+const globalErrorhandler = (err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+  
+    return res.status(statusCode).json({
+      message: err.message,
+      errorStack: config.env === "development" ? err.stack : "",
+    });
+  }
+
+  export default globalErrorhandler;
