e90419156db2ae43b697d2f55f3a27fb0acccf9e
PawanK06 <pawan06kumar2003@gmail.com>
2024-06-08 18:28:31 +0530

    add list book endpoint

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBook } from "./bookController";
+import { createBook, updateBook, listBook } from "./bookController";
 import multer from "multer";
 import path from "node:path";
 import authenticate from "../middlewares/authenticate";
@@ -20,5 +20,15 @@ bookRouter.post(
   ]),
   createBook
 );
+
+bookRouter.patch("/:bookId", authenticate,
+upload.fields([
+  { name: "coverImage", maxCount: 1 },
+  { name: "file", maxCount: 1 },
+]),
+updateBook
+)
+
+bookRouter.get("/", listBook)
 
 export default bookRouter;
