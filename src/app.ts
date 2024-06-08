9ee6158f70aee02c1873db774bb9bf06b3fcf854
PawanK06 <pawan06kumar2003@gmail.com>
2024-06-08 15:42:26 +0530

    add book model

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import createHttpError, { HttpError } from "http-errors";
 import { config } from "./config/config";
 import globalErrorhandler from "./middlewares/globalErrorhandler";
 import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/api/users/", userRouter);
+app.use("/api/books/", bookRouter);
 
 // Global error handler
 
---
+++ src/book/bookModel.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { Book } from "./bookTypes";
+
+const bookSchema = new mongoose.Schema<Book>({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    coverImage: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+    file: {
+        type: String,
+        required: true,
+    }
+}, {timestamps: true});
+
+export default mongoose.model<Book>("Book", bookSchema)
